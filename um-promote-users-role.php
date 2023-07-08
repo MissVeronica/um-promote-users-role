89f6fe49f021e41ea45854e6826682bfd43e81c8
MissVeronica <veronica@tranza.se>
2023-07-08 11:35:23 +0200

    Update um-promote-users-role.php

--- um-promote-users-role.php
+++ um-promote-users-role.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     Ultimate Member - Promote Users Role
  * Description:     Extension to Ultimate Member for User Role Promotions in the frontend Profile Page with Roles dropdown made by Users with WP 'promote_users' capability like a site Administrator.
- * Version:         1.2.0
+ * Version:         2.0.0
  * Requires PHP:    7.4
  * Author:          Miss Veronica
  * License:         GPL v2 or later
@@ -10,7 +10,7 @@
  * Author URI:      https://github.com/MissVeronica
  * Text Domain:     ultimate-member
  * Domain Path:     /languages
- * UM version:      2.5.0
+ * UM version:      2.6.7
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
@@ -18,7 +18,7 @@ if ( ! class_exists( 'UM' ) ) return;
 
 class UM_Promote_Users_Role {
 
-    public $form_ids = array();
+    private $form_ids = array();
 
     function __construct( ) {
 
@@ -29,62 +29,55 @@ class UM_Promote_Users_Role {
 
                 $this->form_ids = array_map( 'trim', array_map( 'sanitize_text_field', explode( ",", $forms )));
 
-                add_filter( 'um_user_edit_profile_fields', array( $this, 'um_user_edit_profile_fields_promote_users_role' ), 999, 2 );
-                add_filter( 'um_submit_form_data',         array( $this, 'um_submit_form_data_promote_users_role' ), 999, 2 );
+                add_filter( 'um_submit_form_data', array( $this, 'um_submit_form_data_promote_users_role' ), 999, 3 );
             }
         }
 
         add_action( 'um_settings_structure', array( $this, 'um_settings_structure_promote_users_role' ), 10, 1 );
     }
 
-    public function um_user_edit_profile_fields_promote_users_role( $fields, $args ) {
-
-        if ( current_user_can( 'promote_users' ) && in_array( $args['form_id'], $this->form_ids ) ) {
-
-            if ( isset( $_POST['role'] ) && ! empty( $_POST['role'] ) ) {
-
-                if ( isset( $fields['role_select']['editable'] )) {
-
-                    // Restore role field for admin editing
-                    $fields['role_select']['editable'] = true;
-
-                    $custom_field_roles = UM()->form()->custom_field_roles( $fields );
-
-                    if ( is_array( $_POST['role'] ) ) {
-
-                        $role = current( $_POST['role'] );
-                        $role = sanitize_key( $role );
-
-                    } else {
-
-                        $role = sanitize_key( $_POST['role'] );
-                    }
-
-                    global $wp_roles;
-
-                    $exclude_roles = array_diff( array_keys( $wp_roles->roles ), UM()->roles()->get_editable_user_roles() );
-
-                    if ( ! empty( $role ) &&
-                        ( ! in_array( $role, $custom_field_roles, true ) || in_array( $role, $exclude_roles, true ) ) ) {
-
-                        wp_die( esc_html__( 'This is not possible for security reasons.', 'ultimate-member' ) );
-                    }
-
-                    $fields['submitted']['role'] = $role;
-                }
-            }
-        }
-
-        return $fields;
-    }
-
-    public function um_submit_form_data_promote_users_role( $post_form, $post_form_mode ) {
+    public function um_submit_form_data_promote_users_role( $post_form, $post_form_mode, $all_cf_metakeys ) {
 
         if ( current_user_can( 'promote_users' ) && $post_form_mode == 'profile' && in_array( $post_form['form_id'], $this->form_ids )) {
 
             if ( isset( $_POST['role'] ) && ! empty( $_POST['role'] ) ) {
 
-                $post_form['submitted']['role'] = sanitize_key( $_POST['role'] );
+                if ( is_array( $_POST['role'] ) ) {
+
+                    $role = current( $_POST['role'] );
+                    $role = trim( sanitize_key( $role ));
+
+                } else {
+
+                    $role = trim( sanitize_key( $_POST['role'] ));
+                }
+
+                if ( empty( $role ) || ! in_array( $role, UM()->roles()->get_editable_user_roles(), true ) ) {
+                    wp_die( esc_html__( 'This is not possible for security reasons.', 'ultimate-member' ) );
+                }
+
+                if ( isset( $_POST['user_id'] ) && ! empty( $_POST['user_id'] ) && is_numeric( $_POST['user_id'] )) {
+
+                    $user_id = intval( $_POST['user_id'] );
+                    $priority_role = UM()->roles()->get_priority_user_role( $user_id );
+
+                    if ( $priority_role !== $role ) {
+
+                        if ( empty( $priority_role ) || ! in_array( $priority_role, UM()->roles()->get_editable_user_roles(), true ) ) {
+                            wp_die( esc_html__( 'This is not possible for security reasons.', 'ultimate-member' ) );
+                        }
+
+                        $new_role = UM()->roles()->set_role( $user_id, $role );
+
+                        if ( ! in_array( $new_role, UM()->roles()->get_editable_user_roles(), true ) ) {
+
+                            UM()->roles()->remove_role( $user_id, $new_role );
+                            wp_die( esc_html__( 'Setting new role failed.', 'ultimate-member' ) );
+                        }
+
+                        UM()->roles()->remove_role( $user_id, $priority_role );
+                    }
+                }
             }
         }
 
@@ -113,4 +106,3 @@ class UM_Promote_Users_Role {
 }
 
 new UM_Promote_Users_Role();
-
